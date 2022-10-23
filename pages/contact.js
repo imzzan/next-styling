0f9763b4aa67fb7dd69e4aa94ef73a79d08a23d7
TugasKu23 <kutugas23@gmail.com>
2022-10-23 10:55:05 +0700

    first commit

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/contact.module.scss';
+
+const Contact = () => {
+  return (
+    <h1 className={styles.lead}>Contact</h1>
+  )
+}
+
+export default Contact
---
+++ pages/css-inline.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CssInline = () => {
+  return (
+    <div>
+        <h1 style={{color : 'blue'}}>Css Inline</h1>
+    </div>
+  )
+}
+
+export default CssInline
